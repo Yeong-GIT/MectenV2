1d944f899bd4b716f5fd5958a607a4446601cb67
Yeong Haoliang <yeonghaoliang0822@gmail.com>
2022-04-04 01:11:58 +0800

    JWT Authentication For Login and Registration

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,6 +1,6 @@
 const Users = require('../models/userModel')
 const bcrypt = require('bcrypt')
-
+const jwt = require('jsonwebtoken')
 
 const userCtrl = {
     register: async(req, res) =>{
@@ -23,13 +23,45 @@ const userCtrl = {
             await newUser.save()
 
             //JasonWebToken(JWT) for Authentication
+            const accesstoken = createAccessToken({id: newUser.id})
+            const refreshtoken = createRefreshToken({id: newUser.id})
 
-            res.json({msg: "Register Success"})
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true, //Use httpOnly to mitigate the risk of client side script accessing the protected cookie
+                path: '/user/refresh_token'
+            })
+
+            res.json({accesstoken})
+            
 
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
+    },
+    refreshToken: (req, res) =>{
+        try{
+            const rf_token = req.cookies.refreshtoken;
+            if(!rf_token) return res.status(400).json({msg:"Please Login or Register"})
+
+            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) =>{
+                if(err) return res.status(400).json({msg:"Please Login or Register"})
+
+                const accesstoken = createAccessToken({id: user.id})
+
+                res.json({accesstoken})
+            })
+
+        }catch(err){
+            return res.status(500).json({msg: err.message})
+        }
     }
+}
+
+const createAccessToken = (user) =>{
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
+}
+const createRefreshToken = (user) =>{
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
 }
 
 module.exports = userCtrl
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,4 +3,6 @@ const userCtrl = require('../controllers/userCtrl')
 
 router.post('/register', userCtrl.register)
 
+router.get('/refresh_token', userCtrl.refreshToken)
+
 module.exports = router 
