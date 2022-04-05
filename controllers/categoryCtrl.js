5ee36cd8b5a5c5a7a5aae5f7cacdd0f81371c2f2
Yeong Haoliang <yeonghaoliang0822@gmail.com>
2022-04-05 16:18:05 +0800

    Admin Category Progress

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -8,7 +8,24 @@ const categoryCtrl = {
         }catch (err) {
             return res.status(500).json({msg: err.message})
         }
+    },
+    createCategory: async (req,res) =>{
+        try{
+            //
+            //only Admin can create, delete and update category
+            const {name} = req.body;
+            const category = await Category.findOne({name})
+            if(category) return res.status.json({msg:"This category has already exists."})
+
+            const newCategory = new Category({name})
+
+            await newCategory.save()
+            res.json({msg: "Created a category"})
+
+        }catch(err){
+            return res.status(500).json({msg: err.message})
+        }
     }
 }
 
-module.exports = categoryCtrl
+module.exports = categoryCtrl 
