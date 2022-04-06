343e58757ac2b316dcd3afc0d273b0760da715fa
Yeong Haoliang <yeonghaoliang0822@gmail.com>
2022-04-06 22:19:45 +0800

    Image Upload and Delete to Cloudinary

--- routes/upload.js
+++ routes/upload.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const cloudinary = require('cloudinary').v2
 const auth = require('../middleware/auth')
 const authSeller = require('../middleware/authSeller')
+const fs = require('fs')
 
 //Upload image on cloudinary 
 cloudinary.config({
@@ -12,14 +13,60 @@ cloudinary.config({
 
 
 //Upload Image
-router.post('/upload', (req, res)=>{
+router.post('/upload',auth, authSeller, (req, res)=>{
     try{
         console.log(req.files)
-        res.json('test upload')
+        if(!req.files || Object.keys(req.files).length === 0)
+            return res.status(400).json({msg:'No files uploaded.'})
+
+        const file = req.files.file;
+        if(file.size > 1024*1024) {
+        removeTmp(file.tempFilePath)
+        return res.status(400).json({msg: "Size too large"})
+        }
+
+        if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
+        removeTmp(file.tempFilePath)
+        return res.status(400).json({msg: "File format is incorrect"})
+        }
+
+        cloudinary.uploader.upload(file.tempFilePath, {folder: "test"},async(err,result)=>{
+            if(err)throw err;
+
+            removeTmp(file.tempFilePath)
+
+            res.json({public_id: result.public_id, url: result.secure_url})
+        })
+
     }catch(err){
-        res.status(500).json({msg: err.message})
+        return res.status(500).json({msg: err.message})
     }
     
 })
+
+//Delete Image
+router.post('/destroy',auth, authSeller, (req, res) =>{
+    try{
+        const {public_id} = req.body;
+        if(!public_id) return res.status(400).json({msg: 'No image Selected'})
+
+        cloudinary.uploader.destroy(public_id, async(err, result) =>{
+            if (err) throw err;
+
+            res.json({msg: "Image has been deleted."})
+
+        })
+
+    }catch (err){
+        return res.status(500).json({msg: err.message})
+    }
+    
+})
+
+const removeTmp = (path) =>{
+    fs.unlink(path, err=>{
+        if (err) throw err;
+    })
+}
 
 module.exports = router
