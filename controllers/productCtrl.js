0fcf167d4bf8985561d512adae5e729dfbea7b58
Yeong Haoliang <yeonghaoliang0822@gmail.com>
2022-04-07 18:51:56 +0800

    Filter Product

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -1,9 +1,51 @@
 const Products = require('../models/productModel')
 
+//Filter, sorting and pagination
+
+class APIfeature{
+    constructor (query, queryString){
+        this.query = query;
+        this.queryString = queryString;
+    }
+    filtering(){
+        const queryObj = {...this.queryString}
+        console.log({before: queryObj})
+        const excludedFields = ['page', 'sort', 'limit']
+        excludedFields.forEach(el => delete(queryObj[el]))
+
+        console.log({after: queryObj})
+
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match)
+        //Query.prototype function is used to specify a $gte query condition. 
+        //.gte() => It returns documents that are greater than or equal the specified condition.
+        //.gt() => It returns documents that are greater than the specified condition.
+        //.lte() => It returns documents that are less than or equal to the specified condition.
+        //.lt() => It returns documents that are less than the specified condition.
+        //.regex() => Filter based on alphabet
+
+         
+        console.log({queryStr})
+        this.query.find(JSON.parse(queryStr))
+
+        return this;
+
+    }
+
+
+    sorting(){}
+
+
+    paginating(){}
+}
+
 const productCtrl = {
     getProducts: async(req,res) => {
         try{
-            const products = await Products.find()
+            const features = new APIfeature(Products.find(), req.query).filtering()
+
+            const products = await features.query
+
             res.json(products)
         }catch(err){
             return res.status(500).json({msg: err.message})
