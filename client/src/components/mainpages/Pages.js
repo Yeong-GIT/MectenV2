39ec10abfaeb019ae5f4a57cbde7eb8e73c7170c
Yeong Haoliang <yeonghaoliang0822@gmail.com>
2022-04-12 03:08:41 +0800

    Product Detail Page Progress

--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Products from './products/Products'
+import DetailProduct from './detailProduct/DetailProduct'
 import Login from './auth/Login'
 import Register from './auth/Register'
 import Cart from './cart/Cart'
@@ -11,13 +12,12 @@ function Pages() {
   return (
     <Routes>
         <Route exact path="/" element={<Products/>}/>
+        <Route exact path="/detail/:id" element={<DetailProduct/>}/>
         <Route exact path="/login" element={<Login/>}/>
         <Route exact path="/register" element={<Register/>}/>
         <Route exact path="/fav" element={<Favourite/>}/>
         <Route exact path="/cart" element={<Cart/>}/>
-
         <Route exact path="*" element={<NotFound/>}/>
-        
     </Routes>
     
   )
--- client/src/components/mainpages/utils/productItem/ProductItem.js
+++ client/src/components/mainpages/utils/productItem/ProductItem.js
@@ -22,7 +22,7 @@ function ProductItem({product}) {
                 Buy
           </Link>
           
-          <Link id = "btn_view" to={'detail/${product._id}'}>
+          <Link id = "btn_view" to={`detail/${product._id}`}>
                 View
           </Link>
 
