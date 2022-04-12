49d8c309c07dab8869899c8e21e48f66c0e1f0ff
Yeong Haoliang <yeonghaoliang0822@gmail.com>
2022-04-13 04:03:12 +0800

    Login Register Link To DB

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,10 +1,22 @@
-import React, {useState, createContext} from 'react'
+import React, {useState, createContext, useEffect} from 'react'
 import ProductsAPI from './api/ProductsAPI'
+import axios from 'axios'
 
 export const GlobalState = createContext()
 
 export const DataProvider = ({children}) =>{
     const [token, setToken] = useState(false)
+
+    const refreshToken = async () =>{
+        const res = await axios.get('/user/refresh_token')
+
+        setToken(res.data.accesstoken)
+    }
+
+    useEffect(() =>{
+        const firstLogin = localStorage.getItem('firstLogin')
+        if(firstLogin) refreshToken()
+    },[])
 
     ProductsAPI()
     const state = {
